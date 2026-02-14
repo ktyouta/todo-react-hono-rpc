1f75833b1ae1d4d6a1944ec3fb8dedf282d9ec7b
ktyouta <ktyouta0506@gmail.com>
2026-02-14 19:05:05 +0900

    Implement core application structure

--- frontend/src/app/components/dashboard-container.tsx
+++ frontend/src/app/components/dashboard-container.tsx
@@ -1,25 +1,74 @@
 import { Dashboard } from "@/components";
 import { paths } from "@/config/paths";
-import { Outlet } from "react-router-dom";
+import { useAppNavigation } from "@/hooks/use-app-navigation";
+import { Navigate, Outlet } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useLogoutMutation } from "../api/logout";
+import { LoginUserContext, SetLoginUserContext } from "./login-user-provider";
 
+
+// サイドバーメニューリスト
+const navigationList = [
+    {
+        name: `ホーム`,
+        icon: (
+            <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-4 0a1 1 0 01-1-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 01-1 1h-2z' />
+            </svg>
+        ),
+        path: paths.todo.path,
+    }
+];
 
 export function DashboardContainer() {
-
-    const navigationList = [
-        {
-            name: `ホーム`,
-            icon: (
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-4 0a1 1 0 01-1-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 01-1 1h-2z' />
-                </svg>
-            ),
-            path: paths.todo.path,
+    // ログインユーザー情報
+    const loginUser = LoginUserContext.useCtx();
+    // ログインユーザー情報(setter)
+    const setLoginUser = SetLoginUserContext.useCtx();
+    // ルーティング用
+    const { appNavigate } = useAppNavigation();
+    // ログアウトリクエスト
+    const mutation = useLogoutMutation({
+        onSuccess: function (): void {
+            setLoginUser(null);
+            appNavigate(paths.login.path);
+        },
+        onError: function (): void {
+            toast.error(`ログアウトに失敗しました。時間をおいて再度お試しください。`)
         }
-    ];
+    });
+
+    if (!loginUser) {
+        return (
+            <Navigate
+                to={paths.login.getHref(location.pathname)}
+                replace
+            />
+        );
+    }
+
+    // ユーザー情報更新画面遷移
+    function moveUserInfoUpdate() {
+        appNavigate(paths.updateUser.path);
+    }
+
+    // パスワード更新画面遷移
+    function movePasswordUpdate() {
+        appNavigate(paths.updatePassword.path);
+    }
+
+    // ログアウト
+    function logout() {
+        mutation.mutate();
+    }
 
     return (
         <Dashboard
             navigationList={navigationList}
+            loginUser={loginUser}
+            moveUserInfoUpdate={moveUserInfoUpdate}
+            movePasswordUpdate={movePasswordUpdate}
+            logout={logout}
         >
             <Outlet />
         </Dashboard>
--- frontend/src/components/layouts/dashboard/dashboard.tsx
+++ frontend/src/components/layouts/dashboard/dashboard.tsx
@@ -1,3 +1,4 @@
+import { LoginUserType } from '@/app/api/verify';
 import { ReactNode, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
@@ -8,10 +9,18 @@ type PropsType = {
         icon?: ReactNode;
         path: string;
     }[];
+    loginUser: LoginUserType;
+    moveUserInfoUpdate(): void;
+    movePasswordUpdate(): void;
+    logout(): void;
 }
 
 export function Dashboard(props: PropsType) {
+
+    // サイドバー開閉フラグ
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    // ユーザーメニュー表示フラグ
+    const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
 
     return (
         <div className='w-full min-h-screen flex bg-gray-100'>
@@ -27,6 +36,7 @@ export function Dashboard(props: PropsType) {
                     </svg>
                 </button>
                 {
+                    // メニューリスト
                     props.navigationList.map((e) => {
                         return (
                             <NavLink
@@ -41,7 +51,7 @@ export function Dashboard(props: PropsType) {
                                 }
                             >
                                 {e.icon && <span className={`shrink-0 ${isSidebarOpen ? 'mr-3' : ''}`}>{e.icon}</span>}
-                                <span className={`transition-opacity duration-300 ${isSidebarOpen ? 'block' : 'hidden'}`}>{e.name}</span>
+                                <span className={`transition-opacity duration-300 text-[16px] ${isSidebarOpen ? 'block' : 'hidden'}`}>{e.name}</span>
                             </NavLink>
                         )
                     })
@@ -49,19 +59,54 @@ export function Dashboard(props: PropsType) {
             </nav>
             <div className='flex flex-col flex-1'>
                 {/* ヘッダー */}
-                <header className='h-14 bg-white border-b border-gray-200 flex items-center px-6'>
-                    <span className='text-[26px] font-bold text-gray-800 tracking-wide'>
+                <header className='h-14 bg-white border-b border-gray-200 flex items-center pl-6 pr-10'>
+                    <span className='text-[26px] font-bold text-gray-800 tracking-wide inline-block flex-1'>
                         Todoリスト
                     </span>
+                    <div className='flex items-center relative'
+                        onClick={() => { setIsUserMenuOpen(true) }}
+                    >
+                        <span className='mr-[10px] text-[18px]'>
+                            {props.loginUser.name}
+                        </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        {/* ユーザーメニュー */}
+                        {
+                            isUserMenuOpen &&
+                            <div className='w-[250px] h-[200px] absolute top-14 right-1 text-[16px] rounded-lg bg-cyan-500 border-black p-5 z-20'>
+                                <span className='block w-fit'
+                                    onClick={props.moveUserInfoUpdate}
+                                >
+                                    ユーザー情報更新
+                                </span>
+                                <span className='block w-fit'
+                                    onClick={props.movePasswordUpdate}
+                                >
+                                    パスワード更新
+                                </span>
+                                <span className='block w-fit'
+                                    onClick={props.logout}
+                                >
+                                    ログアウト
+                                </span>
+                            </div>
+                        }
+                    </div>
+                    {
+                        isUserMenuOpen &&
+                        <div className='absolute w-screen h-screen top-0 z-10 left-0'
+                            onClick={() => { setIsUserMenuOpen(false) }}
+                        />
+                    }
                 </header>
                 <div className='flex flex-1'>
-
                     {/* メインコンテンツ */}
                     <main className='flex-1 flex flex-col p-6'>
                         {props.children}
                     </main>
                 </div>
-
             </div>
         </div>
     );
