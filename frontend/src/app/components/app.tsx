8480b2d832a15e4970301e188997464c941dd280
ktyouta <ktyouta0506@gmail.com>
2026-04-20 23:12:30 +0900

    Fix status bar overlap on mobile devices

--- frontend/src/app/components/app.tsx
+++ frontend/src/app/components/app.tsx
@@ -24,7 +24,7 @@ export function App() {
 
   return (
     <BrowserRouter>
-      <div className="sticky top-0 left-0 right-0 bg-white z-[9999] h-[44px] md:hidden" />
+      <div className="sticky top-0 left-0 right-0 bg-white z-[9999] h-[18px] md:hidden" />
       <ErrorBoundary
         FallbackComponent={Errors}
       >
